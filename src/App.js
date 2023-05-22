ba18c5cded19655e55f7154a1512951a2914857b
Vinay Sayimpu <vinaysayimpu@gmail.com>
2023-05-22 09:43:05 +0000

    Image

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import ProfileCard from './ProfileCard';
 function App() {
   return (
     <div className="App">
-      <ProfileCard />
+      <ProfileCard title="alexa" handle="@alexa99"/>
+      <ProfileCard title="bixby" handle="@bixby99"/>
+      <ProfileCard title="siri" handle="@siri99"/>
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,7 +1,19 @@
-function ProfileCard() {
+function ProfileCard(props) {
+    // const title = props.title;
+    // const handle = props.handle;
+    // above can be written as below single line
+    // const {title, handle} = props;
+
+    // we can write function ProfileCard(props) as
+    // function ProfileCard({ title, handle })
     return (
-        <h1>profileCard is working fine</h1>
-    )
+        <>
+        <div>Title is {props.title}</div>
+        <div>Handle is {props.handle}</div>
+        </>
+        // <div> Title is {title}</div>
+        // <div> Handle is {handle} </div>
+    );
 }
 
 export default ProfileCard;
